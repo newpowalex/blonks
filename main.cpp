e36a0de5b9e39d17685bf3ede50348732f1c1c67
newpowalex <newpowalex@gmail.com>
2023-06-11 22:53:07 -0400

    Added a redirect for debugging output to a .log

--- main.cpp
+++ main.cpp
@@ -2,23 +2,30 @@
 // Headers
 ////////////////////////////////////////////////////////////
 #include <iostream>
+#include <fstream>
 #include "Game.h"
 
 int main()
 {
+    // Redirect output to a file
+    std::ofstream outputFile("output.log");
+    std::streambuf* coutBuffer = std::cout.rdbuf(outputFile.rdbuf());
+    std::streambuf* cerrBuffer = std::cerr.rdbuf(outputFile.rdbuf());
+
     //Init game engine
     Game game;
+    std::cout << "Started";
 
     //Game loop
     while (game.running())
     {
         //Update
         game.update();
+        std::cout << "updating";
 
         //Render
         game.render();
     }
-
 
     //End of application
     return 0;
